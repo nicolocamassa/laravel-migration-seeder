0fa088d5820edb26937b732efbfeaa837cee58a5
nicolocamassa <camassanicolo03@gmail.com>
2024-02-15 19:24:48 +0100

    [UPDATE] print trains that leave today

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -7,8 +7,8 @@ use App\Models\Train;
 class TrainController extends Controller
 {
     public function index(){
-        $trains = Train::all();
-        
+        $trains = Train::where('departure_time', 'LIKE' ,'%2024-02-15%')->get();
+
         return view('index', compact('trains'));
     }
 }
