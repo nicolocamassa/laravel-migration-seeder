b90d4fea2a1b0b288be7341f1e9fb56284a8a478
nicolocamassa <camassanicolo03@gmail.com>
2024-02-15 18:26:19 +0100

    [ADD] Seeder for trains table added

---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train as Train; 
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
